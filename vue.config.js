92314a04d2ef85ff25ffcb45656d6a05b17986ee
cottongrass0828 <inoriro.ame@gmail.com>
2023-11-14 15:29:59 +0800

    publicPath edit

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/data_add_serial_number/'
+    : '/',
   chainWebpack: (config) => {
     config
       .plugin('html')
